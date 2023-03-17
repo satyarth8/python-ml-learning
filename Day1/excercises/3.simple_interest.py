a26f287e2ec6f2eef8d815695070146cdad4d2a6
satyarth8 <satyarthaprakash7168@gmail.com>
2023-03-17 12:02:16 +0530

    learnt about if else and while in python

--- Day1/excercises/3.simple_interest.py
+++ Day1/excercises/3.simple_interest.py
@@ -1,6 +1,9 @@
 p=int(input("Enter the principle : "))
 r=int(input("Enter the rate : "))
 t=int(input("Enter the time : "))
-# we take the input of principal rate and time
+# we take the input of principal ,rate and time
+
 si=p*r*t/100.0 # calculate the simple interest
+
+#displayt the result
 print("Simple Interest : ",si)
