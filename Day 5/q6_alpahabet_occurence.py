d2dfdbad5d7697c30a65ee39d9ad32bad4f5eb05
satyarth8 <satyarthaprakash7168@gmail.com>
2023-03-20 12:54:42 +0530

    updated comment

--- Day 5/q6_alpahabet_occurence.py
+++ Day 5/q6_alpahabet_occurence.py
@@ -1,4 +1,5 @@
 #6. Wap to count  occurances of all alphabets within a given string
+# used the similar concept of hashmap present in c++ in here
 alpha_freq={}
 s=str(input("Enter the string : "))
 x=0
@@ -7,6 +8,6 @@ for i in s:
 for i in s:
     alpha_freq[str(i)]=alpha_freq[str(i)]+1
 
-print("Occurence of all the alphabet is : ")
+print("Occurence of all the alphabet present in string is : ")
 for a,b in alpha_freq.items():
-    print(a,":",b,"times")
+    print(a,":",b,"times",end="\n")
