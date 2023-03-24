21f18bb03592a89650149c401fbc912d33cbe0b2
satyarth8 <satyarthaprakash7168@gmail.com>
2023-03-24 20:01:53 +0530

    folder changes  and new excercises

---
+++ Day 1/NIELIT/List.py
@@ -0,0 +1,7 @@
+a=[1,2,3]
+# changeable , can have any type of element
+print(type(a))
+a=[1,2,3,4,"hello",[12,23]]
+print(a[5][1])# accessing a list inside list
+
+
---
+++ Day 1/NIELIT/List_Methods.py
@@ -0,0 +1,21 @@
+
+#APPEND Element
+#add elements in list 
+b=[]
+b.append('1') # adds element in the list 
+print(b)
+
+#INSERT Elment
+#insert the value in list.insert(positio,value_to_be_added)
+b.insert(1,'2')
+print("After insert a value ",b)
+
+
+#SORT ELement
+b.sort(reverse=True)
+print("After reverse ",b)
+
+#POP Element
+b.pop()
+#removes the last element in the list
+
